f58efab3e462c3f14730d2b8181020350ef71f79
Francho <fran>
2020-08-16 16:30:39 -0300

    serve movies.html

--- movies/tests.py
+++ movies/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.test import SimpleTestCase, TestCase
+from django.urls import reverse
+
+from . import views
 
 # Create your tests here.
+
+class MoviesTests(SimpleTestCase):
+
+    def test_found(self):
+        response = self.client.get('/movies/')
+        self.assertEquals(response.status_code, 200)
+
+    def test_template(self):
+        self.assertTemplateUsed(self.client.get('/movies/'), 'movies/movies.html')
+
